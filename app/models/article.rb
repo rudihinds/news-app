2623f17e087b3490a39754523873e182fe1a36e5
mbarrins <maija.barrins@gmail.com>
2019-07-25 16:14:45 +0100

    added getTopHeadlines Article class method and removed validates presence for author, description, title, content, & url to image from Article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,31 @@
 class Article < ApplicationRecord
   belongs_to :source
+  
+  validates :url, :published_at, presence: true
 
-  validates :author, :description, :title, :url, :url_to_image, :published_at, :content, presence: true
+  def self.getTopHeadlines(sources = Source.all.map{|source| source.api_id})
+    
+    articles = JSON.parse(RestClient.get("https://newsapi.org/v2/top-headlines?sources=#{sources.join(',')}&apiKey=#{ENV["API_KEY"]}"))['articles']
+    
+    articles.each do |article|
+      article["source"] = Source.find_by(api_id: article.delete("source")['id'])
+      article["url_to_image"] = article.delete("urlToImage")
+      article["published_at"] = article.delete("publishedAt")
+    end
+    
+    errors = []
+
+    articles.each do |article|
+      newArticle = Article.find_or_create_by(article)
+
+      if !newArticle.valid?
+        article['errors'] = newArticle.errors.full_messages
+        errors << article
+      end
+    end
+
+    articles
+
+  end
 
 end
