98dede9bedab6375e489cbee3f83d076f64aae01
mbarrins <maija.barrins@gmail.com>
2019-07-25 16:20:26 +0100

    added some folders for react

---
+++ client/src/adapters/API.js
@@ -0,0 +1,4 @@
+const apiEndpoint = 'http://localhost:3000'
+const articlesURL = 'http://localhost:3000/articles'
+const sourcesURL = 'http://localhost:3000/sources'
+const usersURL = 'http://localhost:3000/users'
