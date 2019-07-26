eddd925a20c73e080cf458c5af9b42d4019fbc52
mbarrins <maija.barrins@gmail.com>
2019-07-26 10:16:53 +0100

    added in basic authorisation

--- client/src/adapters/API.js
+++ client/src/adapters/API.js
@@ -1,16 +1,81 @@
 const apiEndpoint = 'http://localhost:3000'
-const articlesURL = 'http://localhost:3000/articles'
-const sourcesURL = 'http://localhost:3000/sources'
-const usersURL = 'http://localhost:3000/users'
+const articlesURL = `${apiEndpoint}/articles`
+const sourcesURL = `${apiEndpoint}/sources`
+const usersURL = `${apiEndpoint}/users`
+
+const jsonify = res => {
+  if (res.ok)
+      return res.json()
+  else
+      throw new Error(res.json())
+}
+const handleServerError = response => console.error(response)
+
+const constructHeaders = (moreHeaders = {}) => (
+  {
+      'Authorization': localStorage.getItem('token'),
+      ...moreHeaders
+  }
+)
 
 const getArticles = () => {
-  return fetch(articlesURL).then(resp => resp.json)
+  return fetch(articlesURL, { headers: constructHeaders() })
+    .then(resp => resp.json)
 }
 
 const getSources = () => {
-  return fetch(sourcesURL).then(resp => resp.json)
+  return fetch(sourcesURL, { headers: constructHeaders() })
+    .then(resp => resp.json)
 }
 
 const getUsers = () => {
-  return fetch(usersURL).then(resp => resp.json)
+  return fetch(usersURL, { headers: constructHeaders() })
+    .then(resp => resp.json)
+}
+
+const signUp = (user) => fetch(signupUrl, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ user })
+}).then(jsonify)
+  .then(data => {
+      localStorage.setItem('token', data.token)
+      return data.user
+  })
+  .catch(handleServerError)
+
+
+const logIn = (user) => fetch(loginUrl, {
+  method: 'POST',
+  headers: {
+      'Content-Type': 'application/json'
+  },
+  body: JSON.stringify({ user })
+}).then(jsonify)
+
+const validateUser = () => {
+  if (!localStorage.getItem('token')) return Promise.resolve({ error: 'no token' })
+
+  return fetch(validateUrl, {
+      headers: constructHeaders()
+  }).then(jsonify)
+      .then(data => {
+          localStorage.setItem('token', data.token)
+          return data.user
+      })
+      .catch(handleServerError)
+}
+
+const clearToken = () => localStorage.removeItem('token')
+
+export default {
+  signUp,
+  logIn,
+  validateUser,
+  clearToken,
+  getUsers,
+  getArticles,
+  getSources
 }
