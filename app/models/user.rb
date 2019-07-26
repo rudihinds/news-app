a99e2a3b93610e2cdc6b524392edc0a5a8b5cebe
mbarrins <maija.barrins@gmail.com>
2019-07-26 09:52:42 +0100

    corrected errors in User associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,13 @@ class User < ApplicationRecord
     has_many :user_sources
     has_many :sources, through: :user_sources
     has_many :user_articles
-    has_many :saved_articles, though: :user_articles, source: articles
-    has_many :sources, though: :user_sources
+    has_many :saved_articles, through: :user_articles, source: :article
+    has_many :sources, through: :user_sources
     has_many :articles, through: :sources
     has_secure_password
 
     validates :first_name, :last_name, :email, :username, presence: true
-    validates :email, username, uniqueness: true
+    validates :email, :username, uniqueness: true
     validates :password, length: { minimimum: 6, maximum: 20 }, allow_nil: true
 
     strip_attributes collapse_spaces: true, replace_newlines: true
